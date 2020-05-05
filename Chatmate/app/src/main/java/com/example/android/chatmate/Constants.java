b1be105fbb1477e974df355aa5721b66652f563f
Manikanta-Ganugula <160030394@kluniversity.in>
2020-05-06 02:14:16 +0530

    added authentication page

--- Chatmate/app/src/main/java/com/example/android/chatmate/Constants.java
+++ Chatmate/app/src/main/java/com/example/android/chatmate/Constants.java
@@ -1,8 +1,8 @@
 package com.example.android.chatmate;
 
 public class Constants {
-    public static String KEY = "3f8b543518e249c5aeccfd84f603e58f";
-    public static String ENDPOINT = "https://tajta150420.cognitiveservices.azure.com";
+    public static String KEY = "050cf2edbc1a4d8b974af8aca355b0cc";
+    public static String ENDPOINT = "https://forchat.cognitiveservices.azure.com/";
     public static int INBOUND = 0;
     public static int OUTBOUND = 1;
 }
--- Chatmate/app/src/main/java/com/example/android/chatmate/MainActivity.java
+++ Chatmate/app/src/main/java/com/example/android/chatmate/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.android.chatmate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -18,55 +25,62 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     static int RC_SIGN_IN = 444;
+    private FirebaseAuth mAuth;
+    private SignInButton signIn;
+    private EditText email = null;
+    private EditText password = null;
+    private String TAG = "Auth";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        createSignInIntent();
-
+        setContentView(R.layout.sign_in);
+        mAuth = FirebaseAuth.getInstance();
+        signIn = findViewById(R.id.button_signin);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email = findViewById(R.id.email_signin);
+                password = findViewById(R.id.password_signin);
+                signIn(email.getText().toString(),password.getText().toString());
+            }
+        });
     }
-    public void createSignInIntent(){
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build());
 
-// Create and launch sign-in intent
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .build(),
-                RC_SIGN_IN);
-
-    }
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-
-            if (resultCode == RESULT_OK) {
-                // Successfully signed in
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show();
-                setContentView(R.layout.activity_main);
-
-                LinearLayout chat_button = (LinearLayout)findViewById(R.id.chat_open);
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
 
 
-                chat_button.setOnClickListener(new View.OnClickListener() {
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onClick(View view) {
-                        Intent i  = new Intent(MainActivity.this, ConversationContainer.class);
-                        startActivity(i);
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                           Intent i= new Intent(MainActivity.this,ConversationContainer.class);
+                           startActivity(i);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                            // [START_EXCLUDE]
+
+                            // [END_EXCLUDE]
+                        }
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "We failed", Toast.LENGTH_SHORT).show();
+                        }
+                        // [END_EXCLUDE]
                     }
                 });
-                // ...
-            } else {
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                // ...
-                Toast.makeText(this, "No Try again", Toast.LENGTH_SHORT).show();
-            }
-        }
+        // [END sign_in_with_email]
     }
+
+
 }
